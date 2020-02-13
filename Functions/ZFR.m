58ce6637eb1ecd9647306fd6c3f63cfd6a8b623d
Tim <zygurt@gmail.com>
2020-02-13 12:00:59 +1000

    Updated filter coefficients
    Added comment at coefficients for the ideal zero-frequency resonator correcting an initial mistake.  a should be [1 -2 1] rather than the [-2 2] that I have included.  However both work, so it is up to the user which the choose to use.

--- Functions/ZFR.m
+++ Functions/ZFR.m
@@ -1,12 +1,16 @@
-function [ y ] = ZFR( s, fs, N_scale )
+function [ y, ZFR_Length ] = ZFR( s, N_CALC, fs, N_scale )
 %[ y ] = ZFR( s, N )
 %   Zero Frequency Resonator
 %
-%   Proposed by Murty and Yegnanarayana, Epoch Extraction from Speech Signals, 2008
+%   Proposed by Murt and Yegnanarayana, Epoch Extraction from Speech Signals, 2008
 %   Implementation based on Rudresh et al., Epoch-Synchronous Overlap-Add
 %   (ESOLA) for Time- and Pitch-Scale Modification of Speech Signals, 2018
 %
 %   s is the speech signal
+%   N_CALC is used to switch between N values used for mean-subtraction operation
+%       0: Speech 44100
+%       1: Calculates fundamental frequency and adjusts accordingly
+%       Otherwise: Baseline fs/220
 %   fs is the sampling frequency of the signal
 %   N_scale is the used to determine where N lies between 1 to 2 times the
 %   average pitch period of the speaker. 1 < N_scale < 2.
@@ -25,26 +29,53 @@ x = s(:)-[0;s(1:end-1)];
 %   G = 1
 %   a1 = -2
 %   a2 = 2
+% Thanks to Novak3 who pointed out that the filter coefficients should be
+% a = [1, -2, 1]
+% This was an error on my part treating a2 and a3 as a1 and a2 as well as a
+% mathematical error as I turned a page.
+% For the true 2 pole ideal zero-frequency resonator, use a=[1,-2,1],
+% however the single pole resonator a=[-2 2] works as well.
 a = [-2 2];
 y1 = filter(1,a,x);
 y2 = filter(1,a,y1);
 
-% Additional Step
-%Find the fundamental pitch period.  Made this up.  Maybe I should find a paper.
-%Pitch period is generally determined using autocorrelation.
-%Because I just want an estimate, and the fundamental is usually loudest,
-%I'm just averaging the entire signal and finding the maximum location.
-ms = 40;
-n = 2^(nextpow2(ms*(10^-3)*fs));
-s_buf = buffer(x,n,3*n/4);
-S = fft(s_buf);
-S_avg = mean(abs(S),2);
-[~,max_loc] = max(S_avg(2:n/2+1));  %ignore DC component
-bin_width = fs/n;
-To = 1/(max_loc*bin_width);
-%Calculate N for mean-subtraction process
-N = round((fs*(N_scale*To-1/fs))/2);
-fprintf('ZFR N value = %d\n',N);
+switch N_CALC
+    case 0
+        N = 217; %For fs=44100 this works passably for male and female speech
+    case 1
+        ms = 40;
+        n = 2^(nextpow2(ms*(10^-3)*fs));
+        s_buf = buffer(x,n,3*n/4);
+        TEMP_max_loc = zeros(size(s_buf,2),1);
+        for m = 1:size(s_buf,2)
+            temp = xcorr(s_buf(:,m),s_buf(:,m));
+            TEMP = fft(temp(n+1:end));
+            [~,TEMP_max_loc(m)] = max(abs(TEMP));
+        end
+        mode_TEMP_max_loc = mode(TEMP_max_loc);
+        voiced_frames = find(TEMP_max_loc<mode_TEMP_max_loc);
+        % S = fft(s_buf(:,voiced_frames(ceil(length(voiced_frames)/2)))); %Just choosing the middle voiced frame
+        S2 = fft(s_buf(:,voiced_frames(:)));
+        S2 = sum(abs(S2),2);
+        % S = abs(S);
+        [~,max_S_loc] = max(S2(2:n/2+1));  %ignore DC component
+        bin_width = fs/n;
+        To = 1/((max_S_loc+1)*bin_width);   %+1 to make up for ignored DC component
+        %Calculate N for mean-subtraction process
+        N = round((fs*(N_scale*To-1/fs))/2);
+    otherwise
+        N = fs/220;
+end
+
+ZFR_Length = N;
+% fprintf('ZFR N value = %d\n',N);
+% figure(1)
+% plot(s)
+% hold on
+% plot(linspace(1,length(s),length(TEMP_max_loc)),TEMP_max_loc/max(TEMP_max_loc))
+% hold off
+% figure(2)
+% plot(S2(1:length(S2)/2))
 
 %Remove trend in y2 by successively applying a mean-subtraction operation.
 %(2N+1) is chosen to lie between 1 to 2 times the average pitch period of the speaker
