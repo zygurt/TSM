e0e2d70f7abadfeb04b9f3e0116247afec8b2830
Tim <zygurt@gmail.com>
2020-06-22 19:00:27 +1000

    Update Inverse percentile function

--- Functions/inv_prctile.m
+++ Functions/inv_prctile.m
@@ -1,13 +1,22 @@
-function [x_per] = inv_prctile(x,V)
+function [x_per] = inv_prctile(x,V,d)
 %Find the closest percentile [0,100] to the given value
 %   x is value
 %   V is vector of known values
+%   d is the direction of improvement 'up' or 'down'
 %   x_per is the closest percentile
 
 if size(V,2)>size(V,1)
     V = V.';
 end
-p = 0:100;
+switch d
+    case 'up'
+        p = 0:100;
+    case 'down'
+        p = 100:-1:0;
+    otherwise
+        fprintf('Direction unknown.  Using greater as improvement\n')
+        p = 0:100;
+end
 P = prctile(V,p);
 dP = P-x;
 [~,x_per] = min(abs(dP));
